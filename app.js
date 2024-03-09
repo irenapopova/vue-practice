da7b139ca3e143a5af18483d6cfbfa8ff7a51ad9
Irena Popova <irenejpopova6@gmail.com>
2024-03-09 10:32:22 +0100

    function added

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const buttonEl = document.querySelector("button");
 const inputEl = document.querySelector("input");
 const listEl = document.querySelector("ul");
 
-function addGoal() {}
+function addGoal() {
+  const enteredValue = inputEl.value;
+  const listItemEl = document.createElement("li");
+  listItemEl.textContent = enteredValue;
+  inputEl.value = "";
+}
 
-buttonEl.addEventListener("click");
+buttonEl.addEventListener("click", addGoal);
