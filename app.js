25b0772bb6a0c8bd555280030fc0608b5237fd26
Irena Popova <irenejpopova6@gmail.com>
2024-03-09 10:20:43 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const buttonEl = document.querySelector("button");
+const inputEl = document.querySelector("input");
+const listEl = document.querySelector("ul");
+
+function addGoal() {}
+
+buttonEl.addEventListener("click");
