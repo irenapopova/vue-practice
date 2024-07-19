04989027991102eeec9fa0d556e732f99e6d0f46
Irena Popova <irenejpopova6@gmail.com>
2024-07-19 11:41:43 +0200

    methods & click events

--- vue-firebase/app.js
+++ vue-firebase/app.js
@@ -1,10 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
-      title: "The Final Empire",
+      title: 'The Final Empire',
       author: 'Brandon Smith'
       age:35
     };
   },
+  methods: {
+    changeTitle() {
+      console.log('you clicked me')
+    }
+  }
 });
 app.mount("#app");
